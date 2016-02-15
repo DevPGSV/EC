8833353c06adcd28de3a98e2cce2e5ede38b9813
DevPGSV <DevPGSV@gmail.com>
2016-02-15 18:24:30 +0100

    Fix comment

--- compare/init.asm
+++ compare/init.asm
@@ -18,9 +18,9 @@ start:
         LDR R2, [R3]    // Load in R2 the value of the address pointed by R3 (Y)
         CMP R1, R2      // Compare the values of R1 and R2
         BLE else        // Go to "else" if the last comparison was true for a "less or equal"
-        STR R1, [R5]    // Store in R5 the value of R1 (R4 <- =X; R1 <- [R4]; [R5] <- R1;)
+        STR R1, [R5]    // Store in the address pointed by R5 the value of R1 (R4 <- =X; R1 <- [R4]; [R5] <- R1;)
         B FIN           // Go to "FIN"
-else:   STR R2, [R5]    // Store in R5 the value of R2 (R3 <- =Y; R2 <- [R3]; [R5] <- R2;)
+else:   STR R2, [R5]    // Store in the address pointed by R5 the value of R2 (R3 <- =Y; R2 <- [R3]; [R5] <- R2;)
 
 
 
